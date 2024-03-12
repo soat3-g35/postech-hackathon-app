e80cfd47a3b53887074c9f79011367b125ac85fa
leonaraqm <162056197+leonaraqm@users.noreply.github.com>
2024-03-12 14:24:11 -0300

    Update LanchoneteApplicationTests.java

--- src/test/java/br/com/fiap/pos/soat3/lanchonete/LanchoneteApplicationTests.java
+++ src/test/java/br/com/fiap/pos/soat3/lanchonete/LanchoneteApplicationTests.java
@@ -1,15 +1,22 @@
 package br.com.fiap.pos.soat3.lanchonete;
 
+import br.com.fiap.pos.soat3.lanchonete.infrastructure.controllers.produto.ProdutoController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 @SpringBootTest
 @EnableFeignClients
 class LanchoneteApplicationTests {
 
+	@Autowired
+	private ProdutoController produtoController;
 	@Test
 	void contextLoads() {
+		assertThat(produtoController).isNotNull();
 	}
 
 }
