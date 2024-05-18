df8d92482bf1fede2dc605abd6ca290c32fdd37f
aline.souza <aline.soouza@live.com>
2024-05-18 18:52:42 -0300

    Unit Test

--- src/test/java/br/com/fiap/pos/soat3/pedido/application/infrastructure/controllers/pedido/PedidoDTOMapperTest.java
+++ src/test/java/br/com/fiap/pos/soat3/pedido/application/infrastructure/controllers/pedido/PedidoDTOMapperTest.java
@@ -12,7 +12,6 @@ import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
